cda074b3bf6f7dd747c9c3b476562d610638e8b8
Eric C. <eyfchoi@gmail.com>
2024-11-03 22:30:32 +0800

    Content. Completed Grid Regeneration Algorithm.

--- script.js
+++ script.js
@@ -15,10 +15,21 @@ change.addEventListener('click', (e) => {
     else
         resolution = Number(prompt("Please enter a Grid Resolution (Min = 1, Max = 100): "));
 
+
+    let gridLength = String((container.clientHeight - 100) / resolution) + "px";
+
     for (let i = 0; i < resolution * resolution; i++)
     {
+        if ((i % resolution) == 0)
+        {
+            let breaks = document.createElement("div");
+            breaks.classList.add("break");
+            container.append(breaks);
+        }
+
         let div = document.createElement("div");
         div.classList.add("grid");
+        div.style.width = div.style.height = gridLength;
         container.append(div);
     }
 });
