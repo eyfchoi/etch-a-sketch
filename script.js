f86403aa98a5b4f19cedbb5276628e912bf1d88b
Eric C. <eyfchoi@gmail.com>
2024-11-03 23:33:34 +0800

    Content. Added Hover Opacity Change and Color Randomizer.

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ let resolution = 16; // Default
 let change = document.querySelector('#change');
 let container = document.querySelector("#container");
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
 change.addEventListener('click', (e) => {
 
     while (container.firstChild)
@@ -14,7 +23,6 @@ change.addEventListener('click', (e) => {
         first = false;
     else
         resolution = Number(prompt("Please enter a Grid Resolution (Min = 1, Max = 100): "));
-
 
     let gridLength = String((container.clientHeight - 100) / resolution) + "px";
 
@@ -30,8 +38,13 @@ change.addEventListener('click', (e) => {
         let div = document.createElement("div");
         div.classList.add("grid");
         div.style.width = div.style.height = gridLength;
+        div.addEventListener('mouseenter', (e) => {e.target.style.backgroundColor = getRandomColor(); e.target.style.opacity -=  '-0.1'; console.log(e.target.style)});//  div.addEventListener('mouseover', (e) => {e.target.style.opacity += 0.1; });
         container.append(div);
     }
 });
+
+// container.addEventListener('mouseover', (e) => {
+//     console.log(e);
+// });
 
 change.dispatchEvent(new Event('click'));
